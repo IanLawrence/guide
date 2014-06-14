f7bff0666124071a0436ec5914b94360358a713c
rdsubhas <rdsubhas@gmail.com>
2014-06-14 12:39:59 +0300

    Updated index page to match theme

--- gitbook-plugin-rapidftr/book/rapidftr.js
+++ gitbook-plugin-rapidftr/book/rapidftr.js
@@ -7,13 +7,13 @@ require(["gitbook", "jQuery"], function(gitbook, jQuery) {
   var $dropdown = $(
     '<ul class="dropdown-menu guides-list" role="menu" aria-labelledby="guide-dropdown"></ul>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Deploy</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/deploy">Deploy</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Administer</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/admin">Administer</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Use</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/use">Use</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Integrate</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/api">Integrate</a></li>'
   );
 
   var $wrapper = $(
