2f184ea4a79088ad172efaaaa55b2e741edeff9b
rdsubhas <rdsubhas@gmail.com>
2014-06-14 13:24:20 +0300

    Added contribute links

--- gitbook-plugin-rapidftr/book/rapidftr.js
+++ gitbook-plugin-rapidftr/book/rapidftr.js
@@ -1,5 +1,6 @@
 require(["gitbook", "jQuery"], function(gitbook, jQuery) {
 
+  // Guides dropdown
   var $link = $(
     '<a class="btn" aria-label="Switch Documentation" id="guide-dropdown" href="#"></a>'
     ).html('<span>Guides</span> <i class="fa fa-angle-down"></i>');
@@ -7,13 +8,13 @@ require(["gitbook", "jQuery"], function(gitbook, jQuery) {
   var $dropdown = $(
     '<ul class="dropdown-menu guides-list" role="menu" aria-labelledby="guide-dropdown"></ul>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/deploy">Deploy</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="https://rapidftr.github.io/guide/deploy">Deploy</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/admin">Administer</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="https://rapidftr.github.io/guide/admin">Administer</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/use">Use</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="https://rapidftr.github.io/guide/use">Use</a></li>'
   ).append(
-    '<li role="presentation"><a role="menuitem" tabindex="-1" href="//rapidftr.github.io/guide/api">Integrate</a></li>'
+    '<li role="presentation"><a role="menuitem" tabindex="-1" href="https://rapidftr.github.io/guide/api">Integrate</a></li>'
   );
 
   var $wrapper = $(
